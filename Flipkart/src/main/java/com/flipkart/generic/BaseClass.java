4252ae9f35a523bfd9739452db15627171631933
CSC <CSC@DESKTOP-1K8K0FM>
2020-08-20 00:15:55 +0530

    Project Commit

---
+++ Flipkart/src/main/java/com/flipkart/generic/BaseClass.java
@@ -0,0 +1,5 @@
+package com.flipkart.generic;
+
+public class BaseClass {
+
+}
---
+++ Flipkart/src/test/java/com/flipkart/Search/SearchProductTest.java
@@ -0,0 +1,16 @@
+package com.flipkart.Search;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SearchProductTest {
+	
+	@Test
+	public void run()
+	{
+		Reporter.log("SearchProductTest Successfuly running",true);
+		Reporter.log("SearchProductTest Successfuly running",true);
+		Reporter.log("SearchProductTest Successfuly running",true);
+	}
+
+}
